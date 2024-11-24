9294d7aa418e5e446e5b4d9472ff2fa448727128
ZhenyaZam <Berlin0516@web.de>
2024-11-24 23:47:42 +0100

    Add files via upload

---
+++ HomeWork  19.11/script.js
@@ -0,0 +1,13 @@
+
+let num = prompt(`Введите любое число`)
+num = Number(num)
+
+if (num===0) {
+    alert(`Число равно нулю`);
+ }
+else if (num%2===0) {
+    alert(`число чётнoе`);
+} 
+ else  {
+    alert(`число нечётнoe`);
+ }
